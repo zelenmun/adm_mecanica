6c95a94c47a68b32af580a527fb91a9f15fe091c
Zelen <oscar_gomez2018@hotmail.com>
2024-12-01 12:34:34 -0500

    .

--- adm/adm_venta.py
+++ adm/adm_venta.py
@@ -9,7 +9,7 @@ from adm.models import Venta
 from adm.forms import ProductoForm
 from core.forms import PersonaForm
 from weasyprint import HTML
-
+import os
 def view(request):
     data = {}
     if request.method == 'POST':
@@ -22,12 +22,21 @@ def view(request):
                 try:
                     fecha = request.GET['fecha'].replace('-', '_')
                     cliente = request.GET['cliente'].replace(' ', '_')
-                    file = fr'../static/pdf/facturas/factura/factura_{fecha}{cliente}.pdf'
-                    path = r'../templates/reportes/factura.html'
-                    # HTML(filename=path).write_pdf(file)
+
+                    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+                    pdf_dir = os.path.join(base_dir, 'static', 'pdf', 'reportes')
+                    file = os.path.join(pdf_dir, f'reporte_{fecha}{cliente}.pdf')
+
+                    os.makedirs(pdf_dir, exist_ok=True)
+
+                    path = os.path.join(base_dir, 'templates', 'reportes', 'factura.html')
+
+                    HTML(filename=path).write_pdf(file)
                     return JsonResponse({"result": True, 'mensaje': 'Ha impreso la factura.'})
                 except Exception as ex:
                     return JsonResponse({"result": False, 'mensaje': 'Ha ocurrido un error generando la factura.', 'detalle': str(ex)})
+
         else:
             try:
                 data['title'] = u'Ventas'
